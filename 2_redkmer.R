1e6649103ecb81d35d971d3ff54ca90468188f43
nwinlo@yahoo.com <nikolai@nikiarch.westgatewifi.local>
2016-09-26 04:09:05 +0100

    add read simulator

--- 2_redkmer.R
+++ 2_redkmer.R
@@ -1,37 +1,18 @@
-## Export the joint_kmer_ratio_name.txt produced in the pipeline
-
+library (ggplot2)
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
-
-pacbio<-read.table("./testproject/counts/pacBio_MappedReads.txt", header=T, sep="\t")
-head(pacbio)
+pacbio<-read.table("./testproject2/counts/pacBio_MappedReads.txt", header=T, sep="\t")
 
 pacbio$candidate[pacbio$CQ>1.5]<-"X"
 pacbio$candidate[pacbio$CQ<1.5]<-"A"
 pacbio$candidate[pacbio$CQ<0.2]<-"Y"
-
-pacbio$adjfactor[pacbio$CQ>1.5]<-1.3333333333
-pacbio$adjfactor[pacbio$CQ<1.5]<-1
-pacbio$adjfactor[pacbio$CQ<0.2]<-4
-
 pacbio$sum<-pacbio$female+pacbio$male
 pacbio$mean<-(pacbio$female+pacbio$male)/2
 pacbio$candidate<-as.factor(pacbio$candidate)
 
-pacbio$sumadj <- pacbio$sum * pacbio$adjfactor
-
-
 summary(pacbio$candidate)
-library (ggplot2)
-
-ggplot(pacbio)+
-  geom_point(aes(x=log10(sumadj), y=CQ,color=candidate),alpha=0.4)
-
-ggplot(pacbio)+
-  geom_point(aes(x=log10(sum), y=CQ,color=candidate),alpha=0.4)
-
-ggplot(pacbio)+
-  geom_point(aes(x=log10(mean), y=CQ,color=candidate),alpha=0.4)
-
-
 summary(pacbio$CQ)
+
+g1 <- ggplot(pacbio) + geom_point(aes(x=log10(sum), y=CQ,color=candidate),alpha=0.4)
+plot(g1)
+
