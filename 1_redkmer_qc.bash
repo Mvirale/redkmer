fcba34b462190992c6063634b0c8f008ff8d4203
Philippos Papathanos <phipapathanos@LaptopPhi2.local>
2016-09-26 22:11:50 -0400

    add qc

--- 1_redkmer_qc.bash
+++ 1_redkmer_qc.bash
@@ -16,25 +16,30 @@ fi
 
 source redkmer.cfg
 
+mkdir -p $CWD/qualityreports
 
+echo "========== quality checking illumina reads and producing report =========="
 
-echo "========== filtering for read quality =========="
+$FASTQC $illF -o $CWD/qualityreports/
+$FASTQC $illM -o $CWD/qualityreports/
 
-##implement QC for reads
-
-mv ${illDIR}/ultraraw_f.fastq ${illDIR}/raw_f.fastq
-mv ${illDIR}/ultraraw_m.fastq ${illDIR}/raw_m.fastq
-
-
-echo "========== filter for mitochondrial DNA =========="
-
-mkdir -p $CWD/index
-
-# Build the index and map the Illumina data
-$BOWTIEB $mtREF $CWD/index/MtRef
-
-# Map the Illumina data on the mito, the option  --un gives the unmapped read (not mitochondrial)
-$BOWTIE -a -p20 -v 0 $CWD/index/MtRef ${illDIR}/raw_f.fastq --un ${illDIR}/f.fastq --al ${illDIR}/f_mito.fastq
-$BOWTIE -a -p20 -v 0 $CWD/index/MtRef ${illDIR}/raw_m.fastq --un ${illDIR}/m.fastq --al ${illDIR}/m_mito.fastq
+# echo "========== filtering for read quality =========="
+# 
+# ##implement QC for reads
+# 
+# mv ${illDIR}/ultraraw_f.fastq ${illDIR}/raw_f.fastq
+# mv ${illDIR}/ultraraw_m.fastq ${illDIR}/raw_m.fastq
+# 
+# 
+# echo "========== filter for mitochondrial DNA =========="
+# 
+# mkdir -p $CWD/index
+# 
+# # Build the index and map the Illumina data
+# $BOWTIEB $mtREF $CWD/index/MtRef
+# 
+# # Map the Illumina data on the mito, the option  --un gives the unmapped read (not mitochondrial)
+# $BOWTIE -a -p20 -v 0 $CWD/index/MtRef ${illDIR}/raw_f.fastq --un ${illDIR}/f.fastq --al ${illDIR}/f_mito.fastq
+# $BOWTIE -a -p20 -v 0 $CWD/index/MtRef ${illDIR}/raw_m.fastq --un ${illDIR}/m.fastq --al ${illDIR}/m_mito.fastq
 
 
