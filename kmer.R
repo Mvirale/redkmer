3d2b0dbd65a73a3fe3d02cf5a0b69abe0714df5b
phipapathanos <p.papathanos@gmail.com>
2016-10-02 18:02:03 -0400

    fixed paths of R plots

--- kmer.R
+++ kmer.R
@@ -15,26 +15,26 @@ summary(kmer$CQ)
 
 g1 <- ggplot(kmer) + geom_point(aes(x=sum, y=CQ,color=candidate),alpha=0.4)+
   scale_color_manual(values=c("springgreen4","red2","dodgerblue2"))
-ggsave("~/Desktop/kmer_1.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_1.png",sep="")),width=13, height=10)
 
 g2<- ggplot(kmer)+geom_histogram(aes(x=CQ),binwidth = 0.05)
-ggsave("~/Desktop/kmer_2.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_2.png",sep="")),width=13, height=10)
 
 g3<- ggplot(kmer)+geom_histogram(aes(x=log10(sum)),binwidth = 0.05)
-ggsave("~/Desktop/kmer_3.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_3.png",sep="")),width=13, height=10)
 
 g4<- ggplot(kmer)+geom_histogram(aes(x=sum),binwidth = 1)
-ggsave("~/Desktop/kmer_4.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_4.png",sep="")),width=13, height=10)
 
 g5 <- ggplot(kmer) + geom_point(aes(x=sum, y=CQ,color=bin),alpha=0.8)+
   scale_color_manual(values=c("grey","black","red2","grey"))+
   ylim(0,3)
-ggsave("~/Desktop/kmer_5.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_5.png",sep="")),width=13, height=10)
 
 g6 <- ggplot(kmer) + geom_point(aes(x=sum, y=CQ, size=log10(offtargets+1),color=bin),alpha=0.8)+
   scale_color_manual(values=c("grey","black","red2","grey"))+
   ylim(0,3)
-ggsave("~/Desktop/kmer_6.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_6.png",sep="")),width=13, height=10)
 
 
 selectSum<-as.numeric(quantile(kmer$sum,c(.999)))
@@ -45,7 +45,7 @@ kmer$selection<-as.factor(kmer$selection)
 g7 <- ggplot(kmer) + geom_point(aes(x=sum, y=CQ, color=selection),alpha=0.8)+
   scale_color_manual(values=c("red2","grey"))+
   ylim(0,3)
-ggsave("~/Desktop/kmer_7.png",width=13, height=10)
+ggsave((paste(Rworkdir,"/kmers/kmer_analysis_7.png",sep="")),width=13, height=10)
 
 candidateXkmers<-subset(kmer,kmer$selection=="good kmers")
 candidateXkmersSeq<-candidateXkmers[,c(1,2)]
